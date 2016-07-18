02b65f84a4f6f03e0c4103c7e23326d13f182ba8
Johannes Dienst <johannesdienst@t-online.de>
2016-07-18 21:24:45 +0200

    One more example; Work on TypeScript Compilation

--- src/js/example.js
+++ src/js/example.js
@@ -1,2 +1,9 @@
+"use strict";
 class Terminator {
 }
+function sugar(p1, p2 = "42", p3) {
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+}
+sugar();
--- src/ts/example.ts
+++ src/ts/example.ts
@@ -1,5 +1,15 @@
+"use strict";
+
 interface Robot { /* TBD */ }
 
 class Terminator implements Robot {
   /* TBD */ 
 }
+
+function sugar(p1, p2="42", p3?) {
+  console.log(p1);
+  console.log(p2);
+  console.log(p3);
+}
+sugar();
+////////////////////////////////////////////////7
