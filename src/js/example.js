93a7c12b87d4660ba11a6cac9021f13ce8235cec
Johannes Dienst <johannesdienst@t-online.de>
2016-07-27 20:47:04 +0200

    Example code for private, protected, public; More types in Presentation: Hybrid Types, Mixins...

--- src/js/example.js
+++ src/js/example.js
@@ -7,3 +7,13 @@ function sugar(p1, p2 = "42", p3) {
     console.log(p3);
 }
 sugar('Hello');
+class MiniRobot {
+    constructor(firstname, lastname, age, gender) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+class NanoRobot extends MiniRobot {
+}
--- src/ts/example.ts
+++ src/ts/example.ts
@@ -13,3 +13,21 @@ function sugar(p1, p2="42", p3?) {
 }
 sugar('Hello');
 ////////////////////////////////////////////////7
+
+class MiniRobot {
+  firstname: string;
+  public lastname: string;
+  protected age: number;
+  private gender: string;
+
+  constructor(firstname: string, lastname: string, age: number, gender: string) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+    this.gender = gender
+  }
+}
+
+class NanoRobot extends MiniRobot {
+
+}
